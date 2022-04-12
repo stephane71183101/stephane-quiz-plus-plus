eb0b911edf0c16d444bc7ae429a3bf4f14d2f559
stephane71183101 <stephane71183101@gmail.com>
2022-04-12 12:16:10 +0200

    first changes

--- index.php
+++ index.php
@@ -11,9 +11,73 @@
     <script src="js/main.js"></script>
 </head>
 <body>
-    <?php
-        echo "Hello, we are starting to work with Databases and PHP PDO!"; 
-    ?>
-    
+<header>
+    <div class="container-fluid">You're playing Quiz++</div>
+</header>
+<main>
+<?php
+echo "Hello, we are starting to work with Databases and PHP PDO!";
+?>
+
+<main>
+    <div>
+        <div class="container-fluid bg-secondary text-light mt-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-1"></div>
+                    <div class="col-10">
+                        <h3 id="questionTitle">First question</h3>
+                        <h6 id="questionWording">Du bist neu in der Schweiz und hast Papierkram zu erledigen. Wonach erkundigt sich das Amt, wenn es fragt: "Werden Sie betrieben?"<h6>
+                    </div>
+                    <div class="col-1"></div>
+                </div>
+            </div>
+        </div>
+        <div class="container text-secondary h6 mt-3">
+            <form action="question-02.php" method="post" onsubmit="return validateQuestion();">       
+                <div class="row">
+                    <div class="col-1"></div>
+                    <div class="col-10" id="answerPanel">
+                        <div class="form-check">
+                            <input type="radio" class="form-check-input" id="single-choice-1" name="single-choice" value="1">
+                            <label class="form-check-label" for="single-choice-1"><p>Haben Sie Schulden?</p></label>
+                        </div>
+                        <div class="form-check">
+                            <input type="radio" class="form-check-input" id="single-choice-2"name="single-choice" value="0">
+                            <label class="form-check-label" for="single-choice-2"><p>Sind Sie selbstständig?</p></label>
+                        </div>
+                        <div class="form-check">
+                            <input type="radio" class="form-check-input" id="single-choice-3" name="single-choice" value="0">
+                            <label class="form-check-label" for="single-choice-3"><p>Haben Sie eine Festanstellung?</p></label>
+                        </div>
+                        <div class="form-check">
+                            <input type="radio" class="form-check-input" id="single-choice-4" name="single-choice" value="0">
+                            <label class="form-check-label" for="single-choice-4"><p>Sind sie haftpflichtversichert?</p></label>
+                        </div>
+                        </div> 
+                        <div class="col-1"></div>
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-1"></div>
+                            <div class="col-10">
+                                <div>
+                                    <input type="hidden" name="lastPageID" value="question-01">
+                                    <input type="hidden" id="achievedPoints" name="achievedPoints">
+                                    <p id="validation-warning" class="warning"></p>
+                                    <button type="submit" class="btn btn-outline-danger buttons">Weiter</button>
+                                </div>
+                            </div>
+                            <div class="col-1"></div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</main>
+</main>
+<footer>
+</footer>
 </body>
 </html>
